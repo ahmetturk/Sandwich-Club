523a0d71bc6e17f6159e02403e213d1f1f871a85
Ahmet Türk <ahmetturk.pc@gmail.com>
2018-02-19 19:27:54 +0300

    bugfix

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,7 +1,6 @@
 package com.udacity.sandwichclub;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
